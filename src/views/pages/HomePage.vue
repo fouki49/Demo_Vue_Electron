96dbea67520360c52800e39adccfecc3e5a83d30
fouki49 <benlemire@hotmail.ca>
2023-08-31 13:41:33 -0400

    first commit

---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <DefaultLayout>
+        <p>Page d'acceuil</p>
+
+    </DefaultLayout>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from "../layouts/DefaultLayout.vue";
+</script>
+
+<style scoped>
+
+</style>
